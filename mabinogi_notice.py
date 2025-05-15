c1aff6da0e7324bd4b340f4462f7e0e9ff085766
DdolKk2 <ddolkk2@gmail.com>
2025-05-15 18:13:12 +0900

    Update mabinogi_notice.py

--- mabinogi_notice.py
+++ mabinogi_notice.py
@@ -29,6 +29,7 @@ options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--lang=ko-KR")
 options.add_argument("user-agent=Mozilla/5.0")
+options.add_argument("window-size=1920x1080")
 
 driver = webdriver.Chrome(options=options)
 driver.get("https://mabinogimobile.nexon.com/News/Notice")
